09501371d5938f23650ca8c358b61dd3cf6fa838
goddessana <twicegoddessana1229@gmail.com>
2024-01-02 08:39:45 +0900

    docs: add sphinx theme

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = 'sewinger'
+copyright = '2024, tgoddessana'
+author = 'tgoddessana'
+release = '0.1.0'
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
+html_static_path = ['_static']
+
+source_suffix = [
+    ".md",
+]
