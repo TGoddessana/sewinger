081e0a3f3fd5e917e5e83810c452e467fc315b1c
goddessana <twicegoddessana1229@gmail.com>
2024-01-05 02:21:21 +0900

    test: commit for test docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ copyright = '2024, tgoddessana'
 author = 'tgoddessana'
 release = '0.1.0'
 
-extensions = ['myst_parser']
+extensions = ['autodoc2', 'sphinx.ext.napoleon', 'myst_parser', ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -13,5 +13,14 @@ html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
 
 source_suffix = [
+    ".rst",
     ".md",
 ]
+
+master_doc = "index"
+
+# autodoc2 settings.
+autodoc2_packages = [
+    "../sewinger/",
+]
+autodoc2_render_plugin = "myst"
--- sewinger/__init__.py
+++ sewinger/__init__.py
@@ -1,2 +1,17 @@
 def some_function():
     print("This is some function")
+
+
+def my_function(arg1, arg2):
+    """
+    This is a brief description of my_function.
+
+    Args:
+        arg1 (int): Description of arg1.
+        arg2 (str): Description of arg2.
+
+    Returns:
+        bool: Description of the return value.
+    """
+    # Function implementation
+    pass
