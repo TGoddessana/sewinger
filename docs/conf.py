c1b823657deb7646ab1f229a99c30d8b7472ad05
goddessana <twicegoddessana1229@gmail.com>
2024-01-06 00:36:02 +0900

    chore: run pre-commit for all source code

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,20 @@
-project = 'sewinger'
-copyright = '2024, tgoddessana'
-author = 'tgoddessana'
-release = '0.1.0'
+project = "sewinger"
+copyright = "2024, tgoddessana"
+author = "tgoddessana"
+release = "0.1.0"
 
-extensions = ['autodoc2', 'sphinx.ext.napoleon', 'myst_parser', ]
+extensions = [
+    "autodoc2",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_permalinks_icon = '<span>#</span>'
-html_theme = 'sphinxawesome_theme'
-html_static_path = ['_static']
+html_permalinks_icon = "<span>#</span>"
+html_theme = "sphinxawesome_theme"
+html_static_path = ["_static"]
 
 source_suffix = [
     ".rst",
